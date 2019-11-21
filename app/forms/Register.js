a32a89faefb7d87ff1f4d35b284f8a53e5af22cd
Juan Andres Baz <juan.baz.pera@gmail.com>
2019-11-21 04:19:56 -0300

    Register props

--- app/forms/Register.js
+++ app/forms/Register.js
@@ -5,13 +5,34 @@ export const RegisterStruct = t.struct({
   name: t.String,
   email: formValidation.email,
   password: formValidation.password,
-  passwordConfirmation: formValidation.password
+  passwordConfirmation: formValidation.password,
 });
 
 export const RegisterOptions = {
   fields: {
     name: {
-      label: 'Nombre (*)'
-    }
-  }
+      label: 'Nombre (*)',
+      placeholder: 'Inserte su nombre completo',
+      error: 'Nombre invalido',
+    },
+    email: {
+      label: 'Email (*)',
+      placeholder: 'Inserte su email',
+      error: 'Email invalido',
+    },
+    password: {
+      label: 'Contraseña (*)',
+      placeholder: 'Inserte contraseña',
+      error: 'Contraseña invalida',
+      password: true,
+      secureTextEntry: true,
+    },
+    passwordConfirmation: {
+      label: 'Repetir contraseña',
+      placeholder: 'Repetir contraseña',
+      error: 'Contraseña invalida',
+      password: true,
+      secureTextEntry: true,
+    },
+  },
 };
