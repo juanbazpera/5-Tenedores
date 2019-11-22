87ebe1adfcf3c715fdfa2c1d87986b3283dcca9b
Juan Andres Baz <juan.baz.pera@gmail.com>
2019-11-22 11:13:04 -0300

    firebase configuracion

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import firebaseConfig from './app/utils/FireBase';
+import { View, StyleSheet } from 'react-native';
 import * as firebase from 'firebase';
 
+import firebaseConfig from './app/utils/FireBase';
 firebase.initializeApp(firebaseConfig);
 
 import UserNavigation from './app/navigations/User';
@@ -17,6 +17,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  }
+    flex: 1,
+  },
 });
