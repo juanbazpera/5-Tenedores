0d0241024c7c542367fa08a283ea8a5e7d9c83eb
Juan Andres Baz <juan.baz.pera@gmail.com>
2019-12-06 01:40:51 -0300

    rules y plugin react hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,30 +2,33 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
   },
   extends: ['airbnb', 'prettier'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   parser: 'babel-eslint',
-  plugins: ['react', 'prettier'],
+  plugins: ['react', 'prettier', 'react-hooks'],
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx'] },
+    ],
     'no-use-before-define': ['error', { variables: false }],
     'react/jsx-wrap-multilines': [
       'error',
-      { arrow: true, return: true, declaration: true }
+      { arrow: true, return: true, declaration: true },
     ],
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn'
-  }
+    'react-hooks/exhaustive-deps': 'warn',
+  },
 };
